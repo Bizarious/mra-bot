891a3211c808ab59937ee5899c59cf6d4536aaea
bizarious <bizardev@web.de>
2021-12-02 18:38:29 +0100

    extended dummy class

--- mra/core/extensions/system.py
+++ mra/core/extensions/system.py
@@ -1,6 +1,12 @@
-from core.ext import extension
+from core.ext import Extension, extension
+from core.commands import command
 
 
-@extension
-class System:
-    pass
+@extension()
+class System(Extension):
+
+    @command()
+    def test(self):
+        print("Hello World")
+
+
