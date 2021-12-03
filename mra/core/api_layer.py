87ca6387c4c7a4cb1a80a691e90d398ef5518a49
bizarious <bizardev@web.de>
2021-12-03 14:29:45 +0100

    added adapter between commands and api

--- mra/core/api_layer.py
+++ mra/core/api_layer.py
@@ -1,11 +1,26 @@
 from abc import ABC, abstractmethod
 from typing import Callable
+from core.commands import Context
 
 
 class APILayer(ABC):
     """
     The template class for the api calling interface.
     """
+
+    def __init__(self, *, prefix: str):
+        self._prefix = prefix
+
+    @property
+    def prefix(self) -> str:
+        return self._prefix
+
+    @prefix.setter
+    def prefix(self, prefix) -> None:
+        self._prefix = prefix
+
+    def on_command_error(self, ctx: Context, exception: Exception) -> None:
+        raise NotImplementedError()
 
     @abstractmethod
     def run(self) -> None:
@@ -15,7 +30,34 @@ class APILayer(ABC):
     def add_message_handler(self, cmd: Callable) -> None:
         pass
 
+    @abstractmethod
+    def create_context(self, *args, **kwargs) -> Context:
+        pass
+
+    @abstractmethod
+    def send_message(self, chat_id: int, message: str) -> None:
+        pass
+
+    @abstractmethod
+    def reply_to_message(self, chat_id: int, message: str, message_id: int) -> None:
+        pass
+
 
 class APICommandAdapter:
-    pass
+    """
+    Adapter for easier communication between api and api layer
+    """
 
+    def __init__(self, api_layer: APILayer, command_handler):
+        self._api_layer = api_layer
+        self._command_handler = command_handler  # no type here because of circular imports
+
+    def translate(self, *args, **kwargs) -> None:
+        context = self._api_layer.create_context(*args, **kwargs)
+        try:
+            self._command_handler.handle_command(context, self._api_layer.prefix)
+        except Exception as e:
+            try:
+                self._api_layer.on_command_error(context, e)
+            except NotImplementedError:
+                print(f"Ignoring Exception:\n{e}")
