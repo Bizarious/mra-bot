c6b159f24a3abfbe7203a8d093211e299fd97139
bizarious <bizardev@web.de>
2021-12-03 14:30:43 +0100

    added adapter to bot

--- mra/core/bot.py
+++ mra/core/bot.py
@@ -1,6 +1,6 @@
-from core import APILayer
+from .api_layer import APILayer, APICommandAdapter
 from core.commands import CommandFeature
-from core.ext import ExtensionHandler, ExtensionClassNotFoundError
+from core.ext import ExtensionHandler
 
 
 class BotExtensionHandler(ExtensionHandler, CommandFeature):
@@ -19,17 +19,20 @@ class Bot:
     """
 
     def __init__(self, api_layer: APILayer):
+        # the api layer, implemented by the used platform
         self.__api_layer = api_layer
+
+        # the extension layer, used to process commands, listeners etc.
         self.__extension_handler = BotExtensionHandler("core/extensions")
 
-    def test(self, _):
-        print("Message")
+        # an adapter to allow communication between command interface and api
+        self.__api_command_adapter = APICommandAdapter(self.__api_layer, self.__extension_handler)
 
-    def test2(self):
+    def test(self):
         self.__extension_handler.load_extensions_from_paths()
         self.__extension_handler.load_extension("System")
 
     def run(self):
-        self.__api_layer.add_message_handler(self.test)
+        self.__api_layer.add_message_handler(self.__api_command_adapter.translate)
         self.__api_layer.run()
 
